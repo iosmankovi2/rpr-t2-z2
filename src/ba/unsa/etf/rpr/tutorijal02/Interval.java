5ddddee523eb4a73837e7f802f76de286729ab3c
iosmankovi2 <iosmankovi2@etf.unsa.ba>
2020-11-03 18:27:29 +0100

    Metode

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -16,7 +16,102 @@ public class Interval {
 
     public Interval() {}
 
+    public boolean isIn(double v)
+    {
+        if(v < pocetna || v > krajnja) return  false;
+        else if(v == pocetna) return  daLiPocetnaPripadaIntervalu;
+        else if(v == krajnja) return daLiKrajnjaPripadaIntervalu;
+        else return false;
+    }
+        public  boolean isNull()
+        {
+            return (pocetna == 0 && krajnja == 0 && daLiPocetnaPripadaIntervalu == false && daLiKrajnjaPripadaIntervalu == false);
+        }
+    public Interval intersect(Interval interval)
+    {
+        Interval presjek = new Interval();
+        if(interval.getPocetnaTacka() >= pocetna) {
 
+            presjek.pocetna = interval.getPocetnaTacka();
+            presjek.daLiPocetnaPripadaIntervalu = interval.getdaLiPocetnaTackaPripadaIntervalu();
+        }
+            else
+            {
+            presjek.krajnja = pocetna;
+            presjek.daLiPocetnaPripadaIntervalu = daLiPocetnaPripadaIntervalu;
 
+        }
+            if(interval.getKrajnjaTacka() <= krajnja)
+            {
+                presjek.krajnja = interval.getKrajnjaTacka();
+                presjek.daLiKrajnjaPripadaIntervalu = interval.daLiKrajnjaPripadaIntervalu;
+            }
 
-}
+            else
+            {
+                presjek.krajnja = krajnja;
+                presjek.daLiKrajnjaPripadaIntervalu = daLiKrajnjaPripadaIntervalu;
+            }
+            return  presjek;
+    }
+
+    private double getKrajnjaTacka() {
+        return  krajnja;
+    }
+
+    private boolean getdaLiPocetnaTackaPripadaIntervalu() {
+        return  daLiPocetnaPripadaIntervalu;
+    }
+
+    private double getPocetnaTacka() {
+        return  pocetna;
+    }
+
+    public static Interval intersect(Interval interval1, Interval interval2)
+    {
+        Interval presjek = new Interval();
+        if(interval1.getPocetnaTacka() <= interval2.getPocetnaTacka())
+        {
+            presjek.pocetna = interval1.getPocetnaTacka();
+            presjek.daLiPocetnaPripadaIntervalu = interval1.getdaLiPocetnaTackaPripadaIntervalu();
+        }
+
+        else
+        {
+            presjek.krajnja = interval2.getPocetnaTacka();
+            presjek.daLiPocetnaPripadaIntervalu = interval2.getdaLiPocetnaTackaPripadaIntervalu();
+        }
+
+        if(interval1.getKrajnjaTacka() <= interval2.getKrajnjaTacka())
+        {
+            presjek.krajnja = interval1.getKrajnjaTacka();
+            presjek.daLiKrajnjaPripadaIntervalu = interval1.daLiKrajnjaPripadaIntervalu;
+        }
+
+        else
+        {
+            presjek.krajnja = interval2.getKrajnjaTacka();
+            presjek.daLiKrajnjaPripadaIntervalu = interval2.getDaLiKrajnjaTackaPripadaIntervalu();
+        }
+        return  presjek;
+    }
+
+    private boolean getDaLiKrajnjaTackaPripadaIntervalu() {
+        return  daLiKrajnjaPripadaIntervalu;
+    }
+
+    @Override
+
+    public String toString()
+    {
+        String s = null;
+        if(this.isNull()) return  "()";
+        if(this.daLiPocetnaPripadaIntervalu == true) s = "[";
+        else s = s + "(";
+        s = s + new Double(this.pocetna).toString() + "," + new Double(this.krajnja).toString();
+        if(this.daLiKrajnjaPripadaIntervalu == true) s = s + "]";
+        else s = s + ")";
+        return  s;
+    }
+
+};
